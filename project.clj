8f372cfdbed5c71c80d4324385fb50625e05e036
Jack Rusher <jack@rusher.com>
2017-07-27 09:47:52 +0200

    Update deps

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
 (defproject spicerack "0.1.3"
-  :description "A Clojure wrapper for MapDB, which is a fast, disk-persistent data-structure library."
+  :description "A Clojure wrapper for MapDB, which is a fast disk-persistent data-structure library."
   :url "https://github.com/jackrusher/spicerack"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.eclipse.collections/eclipse-collections-api "7.1.0"]
-                 [org.eclipse.collections/eclipse-collections "7.1.0"]
-                 [org.eclipse.collections/eclipse-collections-forkjoin "7.1.0"]
+                 [org.eclipse.collections/eclipse-collections-api "7.1.1"]
+                 [org.eclipse.collections/eclipse-collections "7.1.1"]
+                 [org.eclipse.collections/eclipse-collections-forkjoin "7.1.1"]
                  [com.google.guava/guava "19.0"]
-                 [org.mapdb/mapdb "3.0.2"
+                 [org.mapdb/mapdb "3.0.5"
                   :exclusions [org.eclipse.collections/eclipse-collections-api
                                org.eclipse.collections/eclipse-collections
                                org.eclipse.collections/eclipse-collections-forkjoin
