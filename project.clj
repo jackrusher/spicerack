1824a3139bc2307d560b141988f5815a0e02f059
Jack Rusher <jack@rusher.com>
2020-01-17 12:31:15 +0100

    Bump deps

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://github.com/jackrusher/spicerack"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.eclipse.collections/eclipse-collections-api "7.1.1"]
-                 [org.eclipse.collections/eclipse-collections "7.1.1"]
-                 [org.eclipse.collections/eclipse-collections-forkjoin "7.1.1"]
-                 [com.google.guava/guava "19.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.eclipse.collections/eclipse-collections-api "10.1.0"]
+                 [org.eclipse.collections/eclipse-collections "10.1.0"]
+                 [org.eclipse.collections/eclipse-collections-forkjoin "10.1.0"]
+                 [com.google.guava/guava "28.2-jre"]
                  [org.jetbrains.kotlin/kotlin-stdlib "1.2.71"] ; see exclusion
-                 [org.mapdb/mapdb "3.0.7"
+                 [org.mapdb/mapdb "3.0.8"
                   :exclusions [org.eclipse.collections/eclipse-collections-api
                                org.eclipse.collections/eclipse-collections
                                org.eclipse.collections/eclipse-collections-forkjoin
